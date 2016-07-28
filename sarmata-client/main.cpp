b4ee40abc40d6349f7c54aefb174cea6418fffbe
Dawid Skurzok <skurzok@agh.edu.pl>
2016-07-28 12:05:54 +0200

    define grammar test, not working

--- sarmata-client/main.cpp
+++ sarmata-client/main.cpp
@@ -24,6 +24,24 @@ int main()
 
     auto stub_ = ASR::NewStub(channel);
     
+    // define gramamr
+    {
+        
+        DefineGrammarRequest req;
+        req.set_name("client_test");
+        DefineGrammarRespone res;     
+        std::cout << "DefineGrammar call" << std::endl;
+        Status status = stub_->DefineGrammar(&context, req, &res);
+        std::cout << "DefineGrammar called" << std::endl;
+        
+        if (!status.ok()) {
+            std::cout << "DefineGrammar rpc failed: " << status.error_message() << std::endl;
+            return false;
+        }
+        
+        std::cout << res.grammarname() << std::endl;
+    }
+    
     std::shared_ptr<ClientReaderWriter<RecognizeRequest, RecognizeResponse> > stream(
         stub_->Recognize(&context));
 
@@ -35,8 +53,11 @@ int main()
         while (true)
         {
             stream->Write(request);
+            std::cout << "message send" << std::endl;
 	        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            break;
         }
+            stream->WritesDone();
     });
 
     RecognizeResponse response;
