0fb826b278d1658a987cc13ab22d5c569d56ae75
Dawid Skurzok <skurzok@agh.edu.pl>
2016-08-12 14:45:38 +0200

    status simplification

--- libsarmata-client/ASRSessionUtils.cpp
+++ libsarmata-client/ASRSessionUtils.cpp
@@ -13,13 +13,10 @@ namespace sarmata
             case PARTIAL_MATCH               : stream << "PARTIAL_MATCH"; break;
             case NO_MATCH                    : stream << "NO_MATCH"; break;
             case NO_INPUT_TIMEOUT            : stream << "NO_INPUT_TIMEOUT"; break;
-            case RECOGNITION_TIMEOUT         : stream << "RECOGNITION_TIMEOUT"; break;
             case GRAMMAR_LOAD_FAILURE        : stream << "GRAMMAR_LOAD_FAILURE"; break;
             case GRAMMAR_COMPILATION_FAILURE : stream << "GRAMMAR_COMPILATION_FAILURE"; break;
             case RECOGNIZER_ERROR            : stream << "RECOGNIZER_ERROR"; break;
             case TOO_MUCH_SPEECH_TIMEOUT     : stream << "TOO_MUCH_SPEECH_TIMEOUT"; break;
-            case URI_FAILURE                 : stream << "URI_FAILURE"; break;
-            case LANGUAGE_UNSUPPORTED        : stream << "LANGUAGE_UNSUPPORTED"; break;
             case CANCELLED                   : stream << "CANCELLED"; break;
             case SEMANTICS_FAILURE           : stream << "SEMANTICS_FAILURE"; break;
             case START_OF_INPUT              : stream << "START_OF_INPUT"; break;
--- proto/run_protoc.sh
+++
@@ -1,7 +0,0 @@
-grpc_root="/opt/grpc/"
-
-$grpc_root/third_party/protobuf/src/protoc asr_service.proto --plugin=protoc-gen-grpc=$grpc_root/bins/opt/grpc_cpp_plugin --cpp_out=../libsarmata-client/
-
-$grpc_root/third_party/protobuf/src/protoc --grpc_out=../libsarmata-client/ --plugin=protoc-gen-grpc=$grpc_root/bins/opt/grpc_cpp_plugin asr_service.proto
-
-#protoc asr_service.proto --plugin=protoc-gen-grpc=../../third_party/grpc/vsprojects/x64/Release/grpc_cpp_plugin.exe --cpp_out=../libsarmata-client/
